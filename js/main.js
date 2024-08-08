8865715a631975e3d6ab20616c30b5e1101bf94d
Alisher3344 <alisher3344ali@gmail.com>
2024-08-08 07:58:51 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// alert(`password == "3344" !!!!!!`);
+// const pass = "3344";
+// let a;
+// let sum = 0;
+// do {
+//   sum++;
+//   a = prompt("Enter the password");
+// } while (a != pass);
+// alert(`Siz parolni ${sum} martta kirittingiz`);
+ AOS.init();
